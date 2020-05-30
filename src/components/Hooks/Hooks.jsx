604ce3b70356a52060537e4c83d7381b35d8b32b
cuiyiming1998 <1269567664@qq.com>
2020-05-30 21:00:37 +0800

    react hooks useContext

--- src/components/Hooks/Hooks.jsx
+++ src/components/Hooks/Hooks.jsx
@@ -1,5 +1,19 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState , useEffect , createContext , useContext} from 'react';
 import { Button } from 'antd';
+
+const CountContext = createContext();
+
+function Counter() {
+    // useContext  解决父子组件传值
+
+    let count = useContext(CountContext);
+
+    return (
+        <div>
+            <h2>count = {count}</h2>
+        </div>
+    )
+}
 
 function Hooks() {
     const [count,setCount] = useState(0);
@@ -15,6 +29,9 @@ function Hooks() {
         <div>
             <p>You clicked {count} times.</p>
             <Button onClick={()=>{setCount(count+1)}}>Click me</Button>
+            <CountContext.Provider value={count}>
+                <Counter />
+            </CountContext.Provider>
         </div>
     )
 }
